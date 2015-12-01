5b3ff490cb75dfed659c5dd1b12eb20611fb9013
Christian A. Rodriguez <ChristianRRL@gmail.com>
2015-11-30 19:40:42 -0500

    Final GetStockClient

--- src/GetStockClient.java
+++ src/GetStockClient.java
@@ -5,20 +5,25 @@ class GetStockClient
 {    
 	static int PORT = 1050;	// Global variable declaring port number
 	static String localhost = "localhost";
-	static String TonyIP = "10.224.2.155";	// Anthony USF Wifi 
+	static String TonyIP = "10.249.65.165";	// Anthony USF Wifi 
 	static String CRodIP = "172.20.10.2";	// Christian Hotspot
 	static String VaddIP = "192.168.1.24";	// Vaddanak Hotspot
 	static String Cwifi = "10.226.1.95"; //Christian wifi
 	static String CHardine = "192.168.0.2";
+	static String TonyChapel = "192.168.0.20";
+	static String ChrisChapel = "192.168.0.5";
+	static String TonyLocalIP = "192.168.0.2";
+	static String actualIP = "10.247.52.47";
 
-	public static void main(String args[]) throws Exception    
+	public static void main(String args[]) throws Exception
 	{
 		boolean state = true;
 		int count = 1;
+		int timeout = 0;
 		
 		while(state)
 		{
-			String ipAddress = CHardine;	// choose IP Address from the global variables
+			String ipAddress = actualIP;	// choose IP Address from the global variables
 			
 			BufferedReader inFromUser = 
 					new BufferedReader(new InputStreamReader(System.in));
@@ -26,27 +31,43 @@ class GetStockClient
 			InetAddress IPAddress = InetAddress.getByName(ipAddress);
 			byte[] sendData = new byte[1024];
 			byte[] receiveData = new byte[1024];
-			String sentence = inFromUser.readLine();
+			String message = inFromUser.readLine();
 			sendData = null;
-			sendData = sentence.getBytes();
+			sendData = message.getBytes();
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, PORT);
 			clientSocket.send(sendPacket);       			
 			
 			clientSocket.setSoTimeout(5000);
-			try{
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			clientSocket.receive(receivePacket);
 			
-			String modifiedSentence = new String(receivePacket.getData());
-			System.out.println("Response Message " + count + ": " + modifiedSentence);
-			
-			sentence = "";
-			modifiedSentence ="";
+			while (timeout < 2)
+			{
+				
+			try{				
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				clientSocket.receive(receivePacket);
+				
+				String modifiedMessage = new String(receivePacket.getData());
+				System.out.println("S-to-C message " + count + " = " + modifiedMessage);
+				
+				message = "";
+				modifiedMessage ="";
+				timeout = 2;
 			}catch(SocketTimeoutException e){
+				timeout++;
+				System.out.println("Sending attempt " + (timeout + 1));
+				clientSocket.send(sendPacket);
+				continue;
+			}
+			catch(IOException e)
+			{
 				continue;
 			}
 			
+			
+			
+			}
+			timeout = 0;
 			count++;
-		} 
+		}
 	}		
 } 
